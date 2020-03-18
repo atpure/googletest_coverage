fc1d666d921f772966b0e9ad10e73a3a022bc8d8
atpure <atpure100@gmail.com>
2020-03-18 05:30:43 +0000

    improve

--- cleanbuild_run.sh
+++ cleanbuild_run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+BASEDIR=$(pwd)
+
+mkdir build
+cd build
+
 if [ -d "Coverage" ]; then
   rm -rf Coverage
 fi
@@ -8,12 +14,15 @@ rm gtestSample
 rm ./unittest/gtestSample_test
 find -name *.gcno -exec rm {} \;
 find -name *.gcda -exec rm {} \;
+cmake ..
 make clean
 make
 ./unittest/gtestSample_test
 
 if [ "$1" == "c" ]; then
-  lcov -c -d ./ -b ./ -o sample.info
+  lcov -c -i -d ./ -o sample_base.info
+  lcov -c -d ./ -o sample_test.info
+  lcov -a sample_base.info -a sample_test.info -o sample.info
   lcov -r sample.info '/usr/include/*' '/unittest/*' -o sample_filtered.info
   genhtml sample_filtered.info --output-directory=Coverage
 fi
